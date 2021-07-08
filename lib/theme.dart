c73c8cf0f651e907c326fe3908b4a6d81b790f3d
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-07-08 21:31:59 +0700

    first commit

---
+++ lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color purpleColor = Color(0xff5843BE);
+Color orangeColor = Color(0xffFF9376);
+Color lightGreyColor = Color(0xffF6F7F8);
+Color greyColor = Color(0xff989BA1);
+Color redColor = Color(0xffFF5B5B);
+
+TextStyle title =
+    GoogleFonts.poppins(fontWeight: FontWeight.w500, fontSize: 24);
+
+TextStyle subtitle = GoogleFonts.poppins(
+    fontWeight: FontWeight.w300, fontSize: 16, color: lightGreyColor);
+
+TextStyle contentTitle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+);
