420440603b98856e63f595b32e02077034c4d5c5
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-07-09 20:17:21 +0700

    add header

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'components/header.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [Header()],
+        ),
+      ),
+    );
   }
 }
